ac6262008d1e15cb0a43bb80003546f3ba58cc97
Zeenat Affrin <128318350+zeenataffrin@users.noreply.github.com>
2024-04-29 16:55:42 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+let userId = 123;
+var userName = "Devin";
+const accountNumber = 43995;
+ 
+
+userId = 432
+userName = "ironman"
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/ 
+
+console.table([userId,userName,accountNumber]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3+4) // we are using node.js not browser
+
+let name = "Zeenat"
+let age = 24
+let isLoggedIn = true
+
+// ******************primitive data types***********************
+// number => 2 to the power 53
+// string => ""
+// boolean => true/false
+// undefinde => 
+// null => standlone value
+// bigint =>
+// symbol => unique 
+
+// **************Non-Primitive****************************** 
+
+// object 
+// function
+// Array
